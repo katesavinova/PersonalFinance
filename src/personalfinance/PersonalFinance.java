329b1ff8a7c40405465f638d4a1ccf3af420adbc
Ekaterina <savinova.katerina@inbox.ru>
2021-01-05 18:27:38 +0400

    Added class with HashMap

--- src/personalfinance/PersonalFinance.java
+++ src/personalfinance/PersonalFinance.java
@@ -5,6 +5,17 @@
  */
 package personalfinance;
 
+import personalfinance.settings.Text;
+import java.awt.Font;
+import java.awt.FontFormatException;
+import java.awt.GraphicsEnvironment;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
  * @author savin
@@ -15,8 +26,17 @@ public class PersonalFinance {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("it is done");
+       init();
+       //System.out.println(Text.get("PROGRAM_NAME"));
+       System.out.println(Arrays.toString(Text.getMonths()));
     }
-    
+    private static void init(){
+        try {
+            Text.init();
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("fonts/Roboto-Light.ttf")));
+        } catch (FontFormatException | IOException ex) {
+            Logger.getLogger(PersonalFinance.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
